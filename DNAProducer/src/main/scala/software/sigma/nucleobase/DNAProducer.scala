50e0f6f5bf8ad453da519b7df934236bdacb994e
Maria Rabyk <mariia.rabyk@gmail.com>
2018-03-09 11:53:33 +0200

    Add aplication.conf for DNAProducer

--- DNAProducer/src/main/scala/software/sigma/nucleobase/DNAProducer.scala
+++ DNAProducer/src/main/scala/software/sigma/nucleobase/DNAProducer.scala
@@ -2,6 +2,7 @@ package software.sigma.nucleobase
 
 import javax.jms.JMSException
 
+import com.typesafe.config.{Config, ConfigFactory}
 import org.slf4j.LoggerFactory
 import ua.com.Publisher
 import ua.com.entity.Nucleotide._
@@ -10,11 +11,16 @@ import ua.com.entity.{DNAGenerator, Nucleotide}
 object DNAProducer extends App {
   private val logger = LoggerFactory.getLogger(this.getClass)
   try {
-    logger.info("DNA producer has started.")
     val generator = new DNAGenerator
     val DNAStream: Stream[Nucleotide] = generator.nucleo(getRandomNucleo, false)
-    val publisher = new Publisher
 
+    val myConf: Config = ConfigFactory.load()
+    val ID: String = myConf.getString("DNAProducer.ID")
+    val url: String = myConf.getString("DNAProducer.url")
+    val topicName: String = myConf.getString("DNAProducer.topicName")
+
+    val publisher = new Publisher(url, topicName, ID)
+    logger.info("DNA producer has started.")
     while (true) {
       DNAStream take 1000 foreach (n => publisher.send(n.nucleo))
     }
@@ -23,5 +29,4 @@ object DNAProducer extends App {
   } catch {
     case ex: JMSException => println(ex.getMessage)
   }
-
 }
