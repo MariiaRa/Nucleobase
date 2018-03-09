9658aaf0ead83f0ccf13fde961afa2cd4b42a1a9
Maria Rabyk <mariia.rabyk@gmail.com>
2018-03-09 18:38:43 +0200

    Add coordinator actor for checking rate

---
+++ DNAValidator/src/main/scala/software/sigma/nucleobase/actors/Coordinator.scala
@@ -0,0 +1,31 @@
+package software.sigma.nucleobase.actors
+
+import akka.actor.{Actor, ActorLogging, Props}
+import com.typesafe.config.{Config, ConfigFactory}
+import org.slf4j.LoggerFactory
+import software.sigma.nucleobase.entity.Rate
+import ua.com.Publisher
+
+object Coordinator {
+  def props: Props = Props[Coordinator]
+}
+
+class Coordinator extends Actor with ActorLogging {
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  val myConf: Config = ConfigFactory.load()
+  val ID: String = myConf.getString("activeMQ.Coordinator.ID")
+  val url: String = myConf.getString("activeMQ.Coordinator.url")
+  val topicName: String = myConf.getString("activeMQ.Coordinator.topicName")
+
+  val regulator = new Publisher(url, topicName, ID)
+  logger.info("Service coordinator has started.")
+
+  override def receive: Receive = {
+    case r: Rate =>
+     // logger.info("DNA producer has started.")
+      if (r.rate > 0.01) {
+        regulator.send("Stop!")
+      }
+  }
+}
