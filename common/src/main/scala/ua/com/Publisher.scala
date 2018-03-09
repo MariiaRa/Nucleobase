b4a2379a4fd7abbd18f2f3e3cfb402f6354c5ebe
Maria Rabyk <mariia.rabyk@gmail.com>
2018-03-09 18:35:05 +0200

    Update common publisher

--- common/src/main/scala/ua/com/Publisher.scala
+++ common/src/main/scala/ua/com/Publisher.scala
@@ -1,17 +1,10 @@
 package ua.com
 
 import javax.jms._
-import com.typesafe.config._
+
 import org.apache.activemq.ActiveMQConnectionFactory
 
-class Publisher {
-
-  val confCommon: Config = ConfigFactory.load("reference.conf")
-  val url: String = confCommon.getString("common-lib.activeMQ.url")
-  val topicName: String = confCommon.getString("common-lib.activeMQ.topicName")
-
-  val myConf: Config = ConfigFactory.load()
-  val ID: String = myConf.getString("DNAProducer.ID")
+class Publisher(url: String, topicName: String, ID: String) {
 
   val connectionFactory = new ActiveMQConnectionFactory(url)
   connectionFactory.setUseAsyncSend(false)
@@ -22,12 +15,12 @@ class Publisher {
   val session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
   val topic: Topic = session.createTopic(topicName)
   val publisher = session.createProducer(topic)
-  publisher.setDeliveryMode(DeliveryMode.PERSISTENT) //
+  publisher.setDeliveryMode(DeliveryMode.PERSISTENT)
 
   def send(a: String): Unit = {
     val textMessage = session.createTextMessage(a)
     publisher.send(textMessage)
-    println("Message sent: " + textMessage.getText + " to " + textMessage.getJMSDestination + " at " + textMessage.getJMSTimestamp)
+    println("Message sent: " + textMessage.getText + " to " + textMessage.getJMSDestination)
   }
 
   def closeConnection(): Unit = {
