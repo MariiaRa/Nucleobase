5821e97ef125a6c6d6d1cc1cbd77ed7f2b807c63
Maria Rabyk <mariia.rabyk@gmail.com>
2018-03-09 18:36:40 +0200

    Add two threads for publisher and consumer

--- DNAMutator/src/main/scala/software/sigma/nucleobase/DNAMutator.scala
+++ DNAMutator/src/main/scala/software/sigma/nucleobase/DNAMutator.scala
@@ -3,30 +3,88 @@ package software.sigma.nucleobase
 import java.util.{Timer, TimerTask}
 
 import com.typesafe.config.{Config, ConfigFactory}
-import org.slf4j.LoggerFactory
-import ua.com.Publisher
-import ua.com.entity.{DNAGenerator, Nucleotide}
 import ua.com.entity.Nucleotide._
+import ua.com.entity.{DNAGenerator, Nucleotide}
+import ua.com.{Publisher, Subscriber}
+
+class MutationPublisher extends Runnable {
+  def run {
+    val generator = new DNAGenerator
+    val nucleoStream: Stream[Nucleotide] = generator.mutation(getRandomNucleo)
+
+    val myConf: Config = ConfigFactory.load()
+    val mutatorID: String = myConf.getString("activeMQ.DNAMutator.ID")
+    val mutatorUrl: String = myConf.getString("activeMQ.DNAMutator.url")
+    val mutatorTopicName: String = myConf.getString("activeMQ.DNAMutator.topicName")
+    val publisher = new Publisher(mutatorUrl, mutatorTopicName, mutatorID)
+
+    val task: TimerTask = new TimerTask() {
+      def run() {
+        nucleoStream take 50 foreach (n => publisher.send(n.nucleo))
+      }
+    }
+    val timer = new Timer()
+    val delay = 0
+    val intevalPeriod = 15 * 1000
+
+    timer.scheduleAtFixedRate(task, delay, intevalPeriod)
+    // publisher.closeConnection()
+  }
+}
+
+class RateReader extends Runnable {
+
+  override def run(): Unit = {
+    val myConf: Config = ConfigFactory.load()
+    val raterID: String = myConf.getString("activeMQ.Coordinator.ID")
+    val raterUrl: String = myConf.getString("activeMQ.Coordinator.url")
+    val raterTopicName: String = myConf.getString("activeMQ.Coordinator.topicName")
+    val rateChecker = new Subscriber(raterUrl, raterTopicName, raterID)
+
+    while (true) {
+      rateChecker.getRate()
+    }
+    // publisher.closeConnection()
+  }
+}
 
 object DNAMutator extends App {
-  private val logger = LoggerFactory.getLogger(this.getClass)
-  val generator = new DNAGenerator
-  val nucleoStream: Stream[Nucleotide] = generator.mutation(getRandomNucleo)
-  val myConf: Config = ConfigFactory.load()
-  val ID: String = myConf.getString("DNAMutator.ID")
-  val url: String = myConf.getString("DNAMutator.url")
-  val topicName: String = myConf.getString("DNAMutator.topicName")
-  val publisher = new Publisher(url, topicName, ID)
-  logger.info("DMA mutator has started.")
-  val task: TimerTask = new TimerTask() {
-    def run() {
-      nucleoStream take 50 foreach (n => publisher.send(n.nucleo))
+
+  val thread1 = new Thread(new MutationPublisher)
+  thread1.start
+
+  val thread2 = new Thread(new RateReader)
+  thread2.start
+
+  /*  private val logger = LoggerFactory.getLogger(this.getClass)
+    val generator = new DNAGenerator
+    val nucleoStream: Stream[Nucleotide] = generator.mutation(getRandomNucleo)
+
+    val myConf: Config = ConfigFactory.load()
+    val mutatorID: String = myConf.getString("activeMQ.DNAMutator.ID")
+    val mutatorUrl: String = myConf.getString("activeMQ.DNAMutator.url")
+    val mutatorTopicName: String = myConf.getString("activeMQ.DNAMutator.topicName")
+    val publisher = new Publisher(mutatorUrl, mutatorTopicName, mutatorID)
+
+    val raterID: String = myConf.getString("activeMQ.Coordinator.ID")
+    val raterUrl: String = myConf.getString("activeMQ.Coordinator.url")
+    val raterTopicName: String = myConf.getString("activeMQ.Coordinator.topicName")
+    val rateChecker = new Subscriber(raterUrl, raterTopicName, raterID)
+
+    logger.info("DMA mutator has started.")
+    val task: TimerTask = new TimerTask() {
+      def run() {
+        nucleoStream take 50 foreach (n => publisher.send(n.nucleo))
+      }
     }
-  }
-  val timer = new Timer()
-  val delay = 0
-  val intevalPeriod = 5 * 1000
-  // schedules the task to be run in an interval
-  timer.scheduleAtFixedRate(task, delay, intevalPeriod)
-  // publisher.closeConnection()
+    val timer = new Timer()
+    val delay = 0
+    val intevalPeriod = 15 * 1000
+
+  /*  if (rateChecker.getRate()) timer.scheduleAtFixedRate(task, delay, intevalPeriod)
+    else timer.cancel()*/
+
+    timer.scheduleAtFixedRate(task, delay, intevalPeriod)
+
+    // publisher.closeConnection()*/
 }
