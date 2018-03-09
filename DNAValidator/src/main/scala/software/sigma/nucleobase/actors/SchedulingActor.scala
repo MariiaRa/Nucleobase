7616bc84d3d85f84d3f33258443cd7e9c8247c2e
Maria Rabyk <mariia.rabyk@gmail.com>
2018-03-09 18:38:59 +0200

    Update file

--- DNAValidator/src/main/scala/software/sigma/nucleobase/actors/SchedulingActor.scala
+++ DNAValidator/src/main/scala/software/sigma/nucleobase/actors/SchedulingActor.scala
@@ -1,6 +1,7 @@
 package software.sigma.nucleobase.actors
 
 import akka.actor._
+
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.duration._
 
@@ -9,6 +10,7 @@ object SchedulingActor {
   case class DNALog(log: List[List[String]])
 
   case object Key
+
   case object Tick
 
   def props(actor: ActorRef): Props = Props(new SchedulingActor(actor))
@@ -20,13 +22,14 @@ class SchedulingActor(actor: ActorRef) extends Actor with ActorLogging with Time
 
   val stream = new ListBuffer[List[String]]
 
-  timers.startPeriodicTimer(Key, Tick, 10.second)
+  timers.startPeriodicTimer(Key, Tick, 30.second)
 
   override def receive: Receive = {
     case batch: String =>
       stream += batch.grouped(2).toList
     case Tick =>
       actor ! DNALog(stream.toList)
+      log.info(s"Size of stream: ${stream.toList.length}")
       stream.clear()
   }
 }
