0d87765eff58a7cd8a4bfbd5f4dd31ce2ec3d940
Maria Rabyk <mariia.rabyk@gmail.com>
2018-03-13 18:23:19 +0200

    Change message for mutator actor

--- DNAMutator/src/main/scala/software/sigma/nucleobase/DNAMutator.scala
+++ DNAMutator/src/main/scala/software/sigma/nucleobase/DNAMutator.scala
@@ -3,7 +3,7 @@ package software.sigma.nucleobase
 import akka.actor.{ActorRef, ActorSystem}
 import akka.stream.ActorMaterializer
 import org.slf4j.LoggerFactory
-import software.sigma.nucleobase.actors.Mutator.Publish
+import software.sigma.nucleobase.actors.Mutator.StartNewTimer
 import software.sigma.nucleobase.actors.MutatorCoordinator.ReadMessage
 import software.sigma.nucleobase.actors.{Mutator, MutatorCoordinator}
 
@@ -19,7 +19,7 @@ object DNAMutator extends App {
   val mutator: ActorRef = system.actorOf(Mutator.props, "Mutator")
   val mutatorCoordinator: ActorRef = system.actorOf(MutatorCoordinator.props(mutator), "CoordinatorOfMutator")
 
-  mutator ! Publish
+  mutator ! StartNewTimer
 
   while (true) {
     mutatorCoordinator ! ReadMessage
