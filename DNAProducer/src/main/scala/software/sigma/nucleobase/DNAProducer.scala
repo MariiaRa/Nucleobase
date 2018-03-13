2311c9d6e07fc94d33a96c27d475841023040e2c
Maria Rabyk <mariia.rabyk@gmail.com>
2018-03-13 18:22:40 +0200

    Add timer to schedule producer

--- DNAProducer/src/main/scala/software/sigma/nucleobase/DNAProducer.scala
+++ DNAProducer/src/main/scala/software/sigma/nucleobase/DNAProducer.scala
@@ -1,18 +1,19 @@
 package software.sigma.nucleobase
 
+import java.util.{Timer, TimerTask}
 import javax.jms.JMSException
 
 import com.typesafe.config.{Config, ConfigFactory}
 import org.slf4j.LoggerFactory
 import ua.com.Publisher
-import ua.com.entity.Nucleotide._
-import ua.com.entity.{DNAGenerator, Nucleotide}
+import ua.com.entity.NucleotideTransition._
+import ua.com.entity.{DNAGenerator, Nucleotides}
 
 object DNAProducer extends App {
   private val logger = LoggerFactory.getLogger(this.getClass)
   try {
     val generator = new DNAGenerator
-    val DNAStream: Stream[Nucleotide] = generator.nucleo(getRandomNucleo, false)
+    val DNAStream: Stream[Nucleotides] = generator.nucleo(getRandomNucleo, false)
 
     val myConf: Config = ConfigFactory.load()
     val ID: String = myConf.getString("DNAProducer.ID")
@@ -21,10 +22,22 @@ object DNAProducer extends App {
 
     val publisher = new Publisher(url, topicName, ID)
     logger.info("DNA producer has started.")
-    while (true) {
-      DNAStream take 1000 foreach (n => publisher.send(n.nucleo))
+
+    val task: TimerTask = new TimerTask() {
+      def run() {
+        DNAStream take 40000 foreach (n => publisher.send(n.nucleo))
+      }
     }
-    publisher.closeConnection()
+    val timer = new Timer()
+    val delay = 0
+    val intevalPeriod = 10 * 1000
+
+    timer.scheduleAtFixedRate(task, delay, intevalPeriod)
+
+    /*while (true) {
+      DNAStream take 10000 foreach (n => publisher.send(n.nucleo))
+    }*/
+  //  publisher.closeConnection()
   } catch {
     case ex: JMSException => println(ex.getMessage)
   }
