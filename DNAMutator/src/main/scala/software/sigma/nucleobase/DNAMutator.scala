d3ec60c6a0dc138e5d45cfd6c79e8b62f83fe0e8
Maria Rabyk <mariia.rabyk@gmail.com>
2018-03-14 15:06:44 +0200

    Update file

--- DNAMutator/src/main/scala/software/sigma/nucleobase/DNAMutator.scala
+++ DNAMutator/src/main/scala/software/sigma/nucleobase/DNAMutator.scala
@@ -7,6 +7,9 @@ import software.sigma.nucleobase.actors.Mutator.StartNewTimer
 import software.sigma.nucleobase.actors.MutatorCoordinator.ReadMessage
 import software.sigma.nucleobase.actors.{Mutator, MutatorCoordinator}
 
+import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.duration._
+
 object DNAMutator extends App {
 
   private val logger = LoggerFactory.getLogger(this.getClass)
@@ -14,14 +17,12 @@ object DNAMutator extends App {
   implicit def system: ActorSystem = ActorSystem("ActorSystem")
 
   implicit val materializer: ActorMaterializer = ActorMaterializer()
-  implicit val executionContext = system.dispatcher
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   val mutator: ActorRef = system.actorOf(Mutator.props, "Mutator")
   val mutatorCoordinator: ActorRef = system.actorOf(MutatorCoordinator.props(mutator), "CoordinatorOfMutator")
 
-  mutator ! StartNewTimer
+  system.scheduler.scheduleOnce(5 seconds, mutator, StartNewTimer)
+  system.scheduler.schedule(0 milliseconds, 2 seconds, mutatorCoordinator, ReadMessage)
 
-  while (true) {
-    mutatorCoordinator ! ReadMessage
-  }
 }
