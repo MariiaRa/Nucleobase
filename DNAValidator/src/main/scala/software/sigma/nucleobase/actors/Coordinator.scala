5c1d9add87cae0cf60cbfd4d37fcd31ef67e4673
Maria Rabyk <mariia.rabyk@gmail.com>
2018-03-14 15:08:30 +0200

    Change actor logic

--- DNAValidator/src/main/scala/software/sigma/nucleobase/actors/Coordinator.scala
+++ DNAValidator/src/main/scala/software/sigma/nucleobase/actors/Coordinator.scala
@@ -3,14 +3,18 @@ package software.sigma.nucleobase.actors
 import akka.actor.{Actor, ActorLogging, Props}
 import com.typesafe.config.{Config, ConfigFactory}
 import org.slf4j.LoggerFactory
+import software.sigma.nucleobase.actors.Coordinator.SaveRate
 import software.sigma.nucleobase.entity.Rate
 import ua.com.Publisher
 
 object Coordinator {
   def props: Props = Props[Coordinator]
+  case class SaveRate(rate: Double)
 }
 
 class Coordinator extends Actor with ActorLogging {
+
+
   private val logger = LoggerFactory.getLogger(this.getClass)
 
   val myConf: Config = ConfigFactory.load()
@@ -21,13 +25,17 @@ class Coordinator extends Actor with ActorLogging {
   val regulator = new Publisher(url, topicName, ID)
   logger.info("Service coordinator has started.")
 
+  var previousRate = 0.0
+
   override def receive: Receive = {
     case r: Rate =>
-     // logger.info("DNA producer has started.")
-      if (r.rate > 0.01) {
+      self ! SaveRate(r.rate)
+      if (r.rate > 0.01 && previousRate < 0.01) {
         regulator.send("Stop")
-      } else {
+      } else if (r.rate < 0.01 && previousRate > 0.01){
         regulator.send("Start")
       }
+    case save: SaveRate =>
+      previousRate = save.rate
   }
 }
